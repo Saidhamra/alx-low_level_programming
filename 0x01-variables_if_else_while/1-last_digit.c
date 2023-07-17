7596a9edcc5d398f16a0aae1f52e712d47f98b08
said hamra <sdhamra@gmail.com>
2023-07-17 15:56:43 -0700

    Hello C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
   * main - Entry point
   * Description: 'get the last degit of a number'
   * Return: always 0
   */
-
 int main(void)
 {
 	int n;
