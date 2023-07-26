4d30b52d0557e0d585618d028fe16a8f262f250f
said hamra <sdhamra@gmail.com>
2023-07-25 17:08:12 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
-void reset to_98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -15,6 +14,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
