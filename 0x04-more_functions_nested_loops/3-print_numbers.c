30a57589e05ec844765b82c9f68883ad094c8d81
said hamra <sdhamra@gmail.com>
2023-07-31 16:30:41 -0700

    nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
-* print_numbers - function that print all numbers from
-* 0 to 9
-*
+* print_numbers - function that print all numbers from 0 to 9
 * Return: returns nothing
 */
 void print_numbers(void)
